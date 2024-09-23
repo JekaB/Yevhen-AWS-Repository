2eb0d1cc79b5500203881764dfaab29d378bd650
Yevhen Bocharov <yevhen.bocharov@bgcpartners.com>
2024-09-24 00:07:17 +0300

    Installing .NET SDK

--- obj/Debug/net8.0/AwsEc2Subtask4.AssemblyInfo.cs
+++ obj/Debug/net8.0/AwsEc2Subtask4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AwsEc2Subtask4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+958b0547b26753abbad473f347fc5d78d767ecbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AwsEc2Subtask4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AwsEc2Subtask4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
